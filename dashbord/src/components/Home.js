d1ed419a8c84f48a65f129a4ea3b6b2b733b7779
Yogesh Bansal <yogeshbansal667@gmail.com>
2026-01-23 23:15:14 +0530

    routes change

--- dashbord/src/components/Home.js
+++ dashbord/src/components/Home.js
@@ -2,18 +2,19 @@ import React from "react";
 
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 const Home = () => {
 
   const[userDetail,setUserDetail]=useState({});
+
   useEffect(()=>{
   fetch(" https://zerodha-clone-oqj7.onrender.com/user").then((res)=>{
   return res.json();
     }).then((data)=>{
   setUserDetail(data);
     })
-  },[]);
+  });
 
   return (
     <>
